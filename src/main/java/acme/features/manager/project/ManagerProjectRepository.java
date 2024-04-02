d95baa667e479b91b5749190aa17afec858a3e19
guillegomezz8 <guigomrom@alum.us.es>
2024-04-02 18:25:18 +0200

    Listado y Detalles de projects para managers

---
+++ src/main/java/acme/features/manager/project/ManagerProjectRepository.java
@@ -0,0 +1,39 @@
+
+package acme.features.manager.project;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.project.Project;
+import acme.entities.project.UserStoryProject;
+import acme.entities.userStory.UserStory;
+import acme.roles.Manager;
+
+@Repository
+public interface ManagerProjectRepository extends AbstractRepository {
+
+	@Query("select p from Project p where p.manager.id = :managerId")
+	Collection<Project> findAllProjectsByManagerId(int managerId);
+
+	@Query("select p from Project p where p.id = :id")
+	Project findOneProjectById(int id);
+
+	@Query("select m from Manager m where m.id = :managerId")
+	Manager findOneManagerById(int managerId);
+
+	@Query("select p from Project p where p.code = :code")
+	Optional<Project> findOneProjectByCode(String code);
+
+	@Query("select up from UserStoryProject up where up.project.id = :projectId")
+	Collection<UserStoryProject> findUserStoryProjectsByProjectId(int projectId);
+
+	@Query("select up.userStory from UserStoryProject up where up.project.id = :projectId")
+	Collection<UserStory> findAllUserStoriesByProjectId(int projectId);
+
+	@Query("select p from Project p")
+	Collection<Project> findAllProjects();
+}
