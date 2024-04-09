cd4134b23024966d3051bd3ce6002227b04f2af3
JOSE MARIA BAQUERO <josbaqrod@alum.us.es>
2024-04-09 21:42:06 +0200

    SponsorshipCreateService

---
+++ src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipCreateService.java
@@ -0,0 +1,89 @@
+
+package acme.features.sponsor.sponsorship;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.client.views.SelectChoices;
+import acme.entities.project.Project;
+import acme.entities.sponsorship.Sponsorship;
+import acme.roles.Sponsor;
+
+@Service
+public class SponsorSponsorshipCreateService extends AbstractService<Sponsor, Sponsorship> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private SponsorSponsorshipRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Sponsorship object;
+		Sponsor sponsor;
+
+		sponsor = this.repository.findSponsorById(super.getRequest().getPrincipal().getActiveRoleId());
+		object = new Sponsorship();
+		object.setPublished(false);
+		object.setSponsor(sponsor);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void bind(final Sponsorship object) {
+		assert object != null;
+
+		int projectId;
+		Project project;
+
+		projectId = super.getRequest().getData("project", int.class);
+		project = this.repository.findProjectById(projectId);
+
+		super.bind(object, "code", "moment", "initialDate", "finalDate", "amount", "type", "email", "link");
+		object.setProject(project);
+	}
+
+	@Override
+	public void validate(final Sponsorship object) {
+		assert object != null;
+
+	}
+
+	@Override
+	public void perform(final Sponsorship object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Sponsorship object) {
+		assert object != null;
+
+		Collection<Project> projects;
+		SelectChoices choices;
+		Dataset dataset;
+
+		projects = this.repository.findAllProjects();
+		choices = SelectChoices.from(projects, "code", object.getProject());
+
+		dataset = super.unbind(object, "code", "moment", "initialDate", "finalDate", "amount", "type", "email", "link", "published");
+		dataset.put("project", choices.getSelected().getKey());
+		dataset.put("projects", choices);
+
+		super.getResponse().addData(dataset);
+	}
+}
--- src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipRepository.java
+++ src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import acme.client.repositories.AbstractRepository;
 import acme.entities.project.Project;
 import acme.entities.sponsorship.Sponsorship;
+import acme.roles.Sponsor;
 
 @Repository
 public interface SponsorSponsorshipRepository extends AbstractRepository {
@@ -21,4 +22,14 @@ public interface SponsorSponsorshipRepository extends AbstractRepository {
 
 	@Query("select s.project from Sponsorship s where s.id =:sponsorshipId ")
 	Collection<Project> findProjectBySponsorshipId(int sponsorshipId);
+
+	@Query("select s from Sponsor s where s.id = :id")
+	Sponsor findSponsorById(int id);
+
+	@Query("select p from Project p where p.id = :id")
+	Project findProjectById(int id);
+
+	@Query("select p from Project p")
+	Collection<Project> findAllProjects();
+
 }
