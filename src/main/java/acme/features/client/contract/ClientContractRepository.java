ed81090ed36eb8abd0d9ea37837cfeadaf005e3b
Angel Neria <angneraca@alum.us.es>
2024-04-15 20:46:25 +0200

    Corrección tras follow up

--- src/main/java/acme/features/client/contract/ClientContractRepository.java
+++ src/main/java/acme/features/client/contract/ClientContractRepository.java
@@ -10,6 +10,7 @@ import acme.client.repositories.AbstractRepository;
 import acme.entities.contract.Contract;
 import acme.entities.progresslog.ProgressLog;
 import acme.entities.project.Project;
+import acme.entities.systemconfiguration.SystemConfiguration;
 import acme.roles.Client;
 
 @Repository
@@ -35,4 +36,10 @@ public interface ClientContractRepository extends AbstractRepository {
 
 	@Query("select pl from ProgressLog pl where pl.contract.id = :id")
 	Collection<ProgressLog> findManyProgressLogsByContractId(int id);
+
+	@Query("select c from Contract c where c.code= :code")
+	Contract findOneContractByCode(String code);
+
+	@Query("select sc from SystemConfiguration sc")
+	SystemConfiguration findActualSystemConfiguration();
 }
